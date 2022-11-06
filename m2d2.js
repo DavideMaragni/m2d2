07768b4fce35c99ae33b050d2d5b885f87af7a66
DavideMaragni <114513997+DavideMaragni@users.noreply.github.com>
2022-11-06 07:54:21 +0100

    Add files via upload

---
+++ m2d2.js
@@ -0,0 +1,57 @@
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* I diversi tipi di datatype sono:
+1) Strongly type sono una variabile definita che non accetta modifiche di nessun tipo e rimmarrano fisse come il nome che si da hai propri figli
+2) Loosely type sono una cariabile definita all'iniaio che può con il tempo variare ad esempio la tua eta
+3) Primitive Types con diversi tipi di contenuti che puoi dare alla variabile quali sono i numeri, le lettere, vero o falso, se non e definito o nullo 
+
+*/
+
+/* ESERCIZIO 2
+ Descrivi cos'Ã¨ un oggetto in JavaScript, con parole tue.
+*/
+
+/* Gli ogetti sono delle variabili conteneti diversi dati nella quale puoi mettere dati di diverse persone per ogni ogetto */
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+let a = 12;
+let b = 20;
+
+console.log(a+b)
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+let x = 12;
+console.log(x)
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+let name = 'Davide';
+console.log(name)
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+console.log (4-x)
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: Ã¨ la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+let name1 = 'Jhon';
+let name2 = 'Jhon';
+
+console.log(name1===name2)
